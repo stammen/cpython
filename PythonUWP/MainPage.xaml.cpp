5365314890f301e90c8294ae954728361e91e130
Dale Stammen <dalestam@microsoft.com>
2017-01-17 20:28:11 -0800

    added uwp project

---
+++ PythonUWP/MainPage.xaml.cpp
@@ -0,0 +1,38 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include "Python.h"
+#include <ppltasks.h>
+
+using namespace PythonUWP;
+
+using namespace Concurrency;
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+    auto task = create_task(create_async([this]
+    {
+        std::string name = "foo";
+        char* argv[1];
+        argv[0] = (char*)name.c_str();
+        Py_Main(1, argv);   
+    }));
+}
